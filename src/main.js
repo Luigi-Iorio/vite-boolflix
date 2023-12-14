892154970077ed145f41b97d2de50710aea7b0aa
Luigi-Iorio <luigi0iorio@gmail.com>
2023-12-15 00:10:49 +0100

    feat: font awesome

--- src/main.js
+++ src/main.js
@@ -2,4 +2,17 @@ import { createApp } from "vue";
 import "./assets/css/style.css";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import icons */
+import { faStar as fasStar } from "@fortawesome/free-solid-svg-icons";
+import { faStar as farStar } from "@fortawesome/free-regular-svg-icons";
+
+/* add icons to the library */
+library.add(fasStar, farStar);
+
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
