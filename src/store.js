957eb48dd3548ca11228c9ee4912884363137de9
Luigi-Iorio <luigi0iorio@gmail.com>
2023-12-14 19:31:33 +0100

    feat: api info serie tv

--- src/store.js
+++ src/store.js
@@ -2,7 +2,9 @@ import { reactive } from "vue";
 
 export const store = reactive({
   tmdbCard: [],
+  tmdbCardTv: [],
   tmdbApi: "https://api.themoviedb.org/3/search/movie",
+  tmdbApiTv: "https://api.themoviedb.org/3/search/tv",
   apiKey: "3302787678ca722a258e9aee580789a5",
   language: "it_IT",
   apiQuery: "",
